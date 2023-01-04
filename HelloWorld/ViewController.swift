c3a35cb64d1be82b656e77e843d456e3478bf1ce
Ramiros73 <Ramiros73@gmail.com>
2023-01-04 17:49:23 +0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Рамиль Хамидуллин on 04.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
